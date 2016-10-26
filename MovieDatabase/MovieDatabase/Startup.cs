82b850362dda8ff9525cf5dd84ebdd5a612a40be
todor-enikov <enigmaenikov@gmail.com>
2016-10-26 19:28:57 +0300

    Initial commit...Adding solution and console application for the project.

---
+++ MovieDatabase/MovieDatabase/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+        }
+    }
+}
