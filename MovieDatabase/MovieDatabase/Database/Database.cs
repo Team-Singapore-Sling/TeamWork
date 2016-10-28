89fced45b7fcb3753f7b1dbaf2eb060b11fbd5c6
todor-enikov <enigmaenikov@gmail.com>
2016-10-28 20:46:09 +0300

    Created folder Database and added class Database.cs which return instance of the database.

---
+++ MovieDatabase/MovieDatabase/Database/Database.cs
@@ -0,0 +1,18 @@
+﻿using MovieDatabase.EntityData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Database
+{
+    public class Database
+    {
+        public MoviesDatabaseOfTeamSingaporeSlingEntities GetInstance()
+        {
+            var database = new MoviesDatabaseOfTeamSingaporeSlingEntities();
+            return database;
+        }
+    }
+}
