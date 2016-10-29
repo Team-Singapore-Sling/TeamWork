4163dfe1feef6648e06ff5df2e4b385309081e7f
todor-enikov <enigmaenikov@gmail.com>
2016-10-29 16:07:05 +0300

    Created interfaces IEmployee.cs, IGenre.cs, IReportMovie.cs.

---
+++ MovieDatabase/MovieDatabase/Interfaces/IReportMovie.cs
@@ -0,0 +1,26 @@
+﻿using MovieDatabase.EntityData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Interfaces
+{
+    public interface IReportMovie
+    {
+        string MovieName { get; set; }
+
+        string Description { get; set; }
+
+        string Duration { get; set; }
+
+        DateTime Year { get; set; }
+
+        List<Genre> Genres { get; set; }
+
+        List<Employee> Directors { get; set; }
+
+        List<Employee> Actors { get; set; }
+    }
+}
