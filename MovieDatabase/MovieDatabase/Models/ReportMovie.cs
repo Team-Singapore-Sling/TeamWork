3fb348a0f8374e555decada37023378cb5dfeeb8
todor-enikov <enigmaenikov@gmail.com>
2016-10-29 16:06:31 +0300

    Added class ReportMovie.cs which contains the data that should be extracted to the pdf file.

---
+++ MovieDatabase/MovieDatabase/Models/ReportMovie.cs
@@ -0,0 +1,27 @@
+﻿using MovieDatabase.EntityData;
+using MovieDatabase.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Models
+{
+    public class ReportMovie : IReportMovie
+    {
+        public string MovieName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Duration { get; set; }
+
+        public DateTime Year { get; set; }
+
+        public List<Genre> Genres { get; set; }
+
+        public List<Employee> Directors { get; set; }
+
+        public List<Employee> Actors { get; set; }
+    }
+}
