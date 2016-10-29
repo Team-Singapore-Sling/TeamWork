7dd6948d53b839f51225c6661742cb9fda1a95f1
todor-enikov <enigmaenikov@gmail.com>
2016-10-29 16:05:34 +0300

    Sync commit. Added package iTextSharp for the PDF generator.

--- MovieDatabase/MovieDatabase/EntityData/Genre.cs
+++ MovieDatabase/MovieDatabase/EntityData/Genre.cs
@@ -11,7 +11,7 @@ namespace MovieDatabase.EntityData
 {
     using System;
     using System.Collections.Generic;
-    
+
     public partial class Genre
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
