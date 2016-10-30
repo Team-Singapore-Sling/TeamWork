3c5e84e87cd22deaaafd9ccf0580f2f54c373a87
todor-enikov <enigmaenikov@gmail.com>
2016-10-30 13:41:34 +0200

    Removed classes for importing the data from MovieDatabase. Created project MovieDatabase.ImportDataFromFiles and added classes for importing data from files.

---
+++ MovieDatabase/MovieDatabase.ImportDataFromFiles/ImportingData/XMLModels/Director.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace MovieDatabase.ImportDataFromFiles.ImportingData.XMLModels
+{
+    public class Director
+    {
+        [XmlElement("FirstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("LastName")]
+        public string LastName { get; set; }
+
+        [XmlElement("Age")]
+        public string Age { get; set; }
+    }
+}
